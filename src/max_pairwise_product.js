32d93ca27a9345943d7ea0a0472056997234199a
Alexander Rogovoy <rogovoyalexandr94@gmail.com>
2020-11-16 21:10:11 +0200

    max_pairwise_product.js v2

--- src/max_pairwise_product.js
+++ src/max_pairwise_product.js
@@ -28,19 +28,34 @@ function readLine (line) {
     process.exit();
 }
 
-function max(arr) {
-    let maxIndex1 = 0, maxIndex2 = -1;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > arr[maxIndex1])
-            maxIndex1 = i;
-    }
+// function max2(arr) {
+//     let maxIndex1 = 0, maxIndex2 = -1;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] > arr[maxIndex1])
+//             maxIndex1 = i;
+//     }
+//
+//     for (let i = 0; i < arr.length; i++) {
+//         if (maxIndex1 !== i && (maxIndex2 === -1 || arr[i] > arr[maxIndex2])) {
+//             maxIndex2 = i;
+//         }
+//     }
+//     return arr[maxIndex2] * arr[maxIndex1];
+// }
 
-    for (let i = 0; i < arr.length; i++) {
-        if (maxIndex1 !== i && (maxIndex2 === -1 || arr[i] > arr[maxIndex2])) {
+
+function max(arr) {
+    let maxIndex1 = 0, maxIndex2 = 1;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[maxIndex1] - arr[i] < 0) {
+            maxIndex2 = maxIndex1;
+            maxIndex1 = i;
+        } else if (arr[maxIndex1] - arr[i] <= arr[maxIndex1] - arr[maxIndex2]) {
             maxIndex2 = i;
         }
     }
-    return arr[maxIndex2] * arr[maxIndex1];
+
+    return arr[maxIndex1] * arr[maxIndex2];
 }
 
 module.exports = max;
