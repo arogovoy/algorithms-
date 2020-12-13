c86b87972b824e7a959fbf8bc8d6ff22be488d95
RogovoyAlexandr <rogovoyalexandr94@gmail.com>
2020-12-13 11:47:34 +0200

    Maximum Salary

--- w3/largest_number.py
+++ w3/largest_number.py
@@ -1,41 +1,40 @@
 # Uses python3
 
 import sys
+from math import log, pow
+from functools import reduce
 
 
 # Maximum Salary
 
-def decreaseValue(a):
-    while a / 10 >= 1:
-        a /= 10
-    return a
+def compare(a, b):
+    order_a = int(log(a, 10))
+    order_b = int(log(b, 10))
+
+    num1 = a * pow(10, order_b + 1) + b
+    num2 = b * pow(10, order_a + 1) + a
+
+    return num2 < num1
 
 
-def sort(arr):
-    pseudo = list(map(decreaseValue, arr))
+def largest_number(arr):
     i = 1
-    while i < len(pseudo):
+    while i < len(arr):
         j = i - 1
-        p = pseudo[i]
         a = arr[i]
-        while j >= 0 and p > pseudo[j]:
-            pseudo[j + 1] = pseudo[j]
+        while j >= 0 and compare(int(a), int(arr[j])):
             arr[j + 1] = arr[j]
             j -= 1
-        pseudo[j + 1] = p
         arr[j + 1] = a
         i += 1
-    return arr
 
-
-def largest_number(a):
-    a = sort(a)
-
-    return a
+    return int(reduce(lambda v1, v2: str(v1) + str(v2), arr))
 
 
 if __name__ == '__main__':
-    # input = sys.stdin.read()
-    # data = input.split()
-    # a = data[1:]
-    print(largest_number([23, 39, 92, 9, 3, 357, 359, 368, 335, 9, 900]))  # 9 92 39 368 359 357 335 3 23
+    input = sys.stdin.read()
+    data = input.split()
+    a = data[1:]
+    print(largest_number(a))
+    # print(largest_number([23, 39, 92, 9, 3, 357, 359, 368, 335, 9, 900]))  # 9 9 92 39 368 359 357 335 3 23
+    # print(largest_number([23, 39, 92]))  # 923923
