5de72851974bf9b3f165cf9ed38e345fcbb24280
RogovoyAlexandr <rogovoyalexandr94@gmail.com>
2021-01-17 19:16:47 +0200

    quick_sort.py

--- w4/quick_sort.py
+++ w4/quick_sort.py
@@ -6,8 +6,38 @@ import random
 # Improving Quick Sort
 
 def partition3(a, l, r):
-    # write your code here
-    pass
+    x = a[l]
+    j = l
+    xs, xe = -1, -1
+    for i in range(l + 1, r + 1):
+        if a[i] < x:
+            if xs != -1:
+                a[xs], a[xe + 1] = a[xe + 1], a[xs]
+                if a[xs] > x:
+                    a[xs], a[i] = a[i], a[xs]
+                xs += 1
+                xe += 1
+            else:
+                j += 1
+                a[i], a[j] = a[j], a[i]
+
+        elif a[i] == x:
+            if xs == -1:
+                j += 1
+                a[i], a[j] = a[j], a[i]
+                xs, xe = j, j
+            else:
+                xe += 1
+                a[xe], a[i] = a[i], a[xe]
+
+    if xs == -1:
+        a[l], a[j] = a[j], a[l]
+        xs, xe = j, j
+    else:
+        xs -= 1
+        a[xs], a[l] = a[l], a[xs]
+
+    return xs, xe
 
 
 def partition2(a, l, r):
@@ -26,10 +56,9 @@ def randomized_quick_sort(a, l, r):
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    # use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1)
-    randomized_quick_sort(a, m + 1, r)
+    xs, xe = partition3(a, l, r)
+    randomized_quick_sort(a, l, xs - 1)
+    randomized_quick_sort(a, xe + 1, r)
 
 
 if __name__ == '__main__':
